d8e51b26d27cfdb38a68018c7473c66e3877ac0a
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-08-27 22:33:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+    let x = {
+        0: [0, 0, 0, 0],
+        1: [1, 0, 0, 0],
+        2: [0, 1, 0, 0],
+        3: [1, 1, 0, 0],
+        4: [0, 0, 1, 0],
+        5: [1, 0, 1, 0],
+        6: [0, 1, 1, 0],
+        7: [1, 1, 1, 0],
+        8: [0, 0, 0, 1],
+        9: [1, 0, 0, 1]
+    };
+    let t = new Date().toString().split(' ')[4].replace(/:/g, '').split('')
+        .map(t => {
+            return parseInt(t);
+        });
+    let incr = n => {
+        t[n]++;
+        if (t[n] == (n % 2 == 1 ? 10 : 6)) {
+            t[n] = 0;
+            if (n) incr(--n);
+        }
+        if (t.join('') == '240000') t = [0, 0, 0, 0, 0, 0];
+        $('#clock >div').each((i, v) => {
+            $(v).find('.dot').each((index, dot) => {
+                $(dot).css('background-color', x[t[i]][index] == 0 ? '#fff' : 'rgba(10, ' + (i + 1) * 42 + ', 255,' + (index + 1) * 0.25 + ')');
+            });
+        });
+
+    };
+    setInterval(() => {
+        incr(5);
+    }, 1000);
+
+});
